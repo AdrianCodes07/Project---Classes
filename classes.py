f46f49e468b0f5fb18521d34c52c4f9eeed82234
AdrianCodes07 <adrian@ortiz.se>
2025-11-12 15:59:05 +0100

    tillagt arena, starta spel

--- classes.py
+++ classes.py
@@ -75,5 +75,64 @@ class Warrior (Karaktar):
         if annan_karaktär.hälsa < 0:
             annan_karaktär.hälsa = 0 
             print(f"{annan_karaktär.namn} har nu {annan_karaktär.hälsa} HP kvar.")
-            
 
+class Mage(Karaktar):
+    def __init__(self, namn):
+        super().__init__(namn, hälsa=80, kraft=25)
+        self.mana = 50
+    
+    def attack(self, annan_karaktär):
+        skada = self.kraft + random.randint(-5, 5)
+        print(f"{self.namn} använder Firestorm och träffar {annan_karaktär.namn} för {skada} skada!")
+        annan_karaktär.hälsa -= skada
+        if annan_karaktär.hälsa < 0:
+            annan_karaktär.hälsa = 0 
+        print(f"{annan_karaktär.namn} har nu {annan_karaktär.häksa} HP kvar.")
+
+class Arena:
+    def __init__(self):
+        self.kar1 = self.välj_karaktär(1)
+        self.kar2 = self.välj_karaktär(2)
+    
+    def välj_karaktär(self, nummer):
+        print(f"\nVälj karaktär #{nummer}:")
+        print("1) Ranger\n2) Warrior\n3 Mage")
+        while True:
+            val = input("Skriv numret för din valda karaktär: ").strip()
+            namn = input("Ge din karaktär ett namn: ").strip()
+            if val == "1":
+                return Ranger(namn)
+            elif val == "2":
+                return Warrior(namn)
+            elif val == "3":
+                return Mage(namn)
+    
+    def starta_strid(self):
+        print(f"Striden börjar mellan {self.kar1.namn} och {self.kar2.namn}")
+        runda = 1 
+        while self.kar1.är_vid_liv() and self.kar2.är_vid_liv():
+            print(f"Runda")
+            self.tur(self.kar1, self.kar2)
+            if not self.kar2.är_vid_liv():
+                print(f"{self.kar1.namn} vinnner kampen!")
+                break
+
+
+            self.tur(self.kar2, self.kar1)
+            if not self.kar1.är_vid_liv():
+                print(f"{self.kar2.namn} vinner kampen!")
+                break
+
+            runda += 1
+            time.sleep(0.7)
+    
+
+    def tur(self, attacker, försvarare):
+        if hasattr(attacker, "specialattack") and random.choice([True, False]):
+            attacker.specialattack(försvarare)
+        else: 
+            attacker.attack(försvarare)
+
+if __name__ == "__main__":
+    arena = Arena()
+    arena.starta_strid()
